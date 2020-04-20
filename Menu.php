c6858ee0c0d6f4acd6a539f3dba5dca07b140222
Ayodeji Ayankola <ayodejiayankola@gmail.com>
2020-04-20 11:02:48 +0100

    instiated the menu class in the StartApp File

--- Menu.php
+++ Menu.php
@@ -1,6 +1,18 @@
 <?php
 
-
+function printMenu(){
+    echo "********** ToDo Console App
+        *******************\n";
+    echo "1 - create a New todo list\n";
+    echo "2 - Show all to do list\n";
+    echo "3 - Show Item in a list\n";
+    echo "4 - Edit List Name\n";
+    echo "5 - Edit List Item\n";
+    echo "6 - Remove a list\n";
+    echo "7 - Remove an item in the list\n";
+    echo "8 - Delete all Todo\n";
+    echo "9 - Quit!\n";
+}
 while (true){
     //print the menu on console
     printMenu();
@@ -55,16 +67,3 @@ while (true){
 
 }
 
-function printMenu(){
-    echo "********** ToDo Console App
-        *******************\n";
-    echo "1 - create a New todo list\n";
-    echo "2 - Show all to do list\n";
-    echo "3 - Show Item in a list\n";
-    echo "4 - Edit List Name\n";
-    echo "5 - Edit List Item\n";
-    echo "6 - Remove a list\n";
-    echo "7 - Remove an item in the list\n";
-    echo "8 - Delete all Todo\n";
-    echo "9 - Quit!\n";
-}
--- startApp.php
+++ startApp.php
@@ -1,16 +1,6 @@
 <?php
 require_once 'Menu.php';
 
-//Created a new instance of the menu
-$newApp = new Menu();
 
 //Run the menu loop to start the print menu functions
-while(true){
-
-    $newApp->printMenu($input);
-
-    if($input === 9){
-        $this->quit;
-    }
-
-}
+printMenu();
